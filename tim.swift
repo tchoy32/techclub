fb0b64c1615db0282aacf492c7ea98b73e348181
Timdiz <Timdiz@Timdizs-MacBook-Air.local>
2018-03-01 19:12:28 -0800

    wow

---
+++ tim.swift
@@ -0,0 +1,18 @@
+print("Hello World")
+
+func glass(boolio:Bool, txt1:String){
+    if boolio{
+        print(txt1)
+    }
+    else {
+        print("dang")
+    }
+}
+
+glass(boolio: true, txt1: "sick dude")
+glass(boolio: false, txt1: "dang dude")
+
+func dog(){
+    print("pug")
+}
+dog()
